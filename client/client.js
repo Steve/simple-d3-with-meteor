af901002b12840a0e2817b1c7f347efc7b31e8f2
Steve Sanderson <mail@stevesanderson.com>
2013-06-07 22:05:21 -0500

    added in new use of d3 (svg) so that we're controlling individual graphical elements via meteor (closer to how meteor manipulates the HTML DOM)

--- client/client.js
+++ client/client.js
@@ -19,6 +19,7 @@ Template.count.rendered = function () {
 					.attr("cx", function(d) { return d * 50})
 					.attr("cy", function(d) { return d * 50})
 					.text("foo")
+					.attr("id", function(d) { return d })
 					.on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
 					.on("mouseout", function(){d3.select(this).style("fill", "white");});
 
@@ -26,3 +27,52 @@ Template.count.rendered = function () {
 		circles.exit().remove();
 	}
 }
+
+Template.vthing.circle = function() {
+	var id = "a" + this._id;
+	var selector_id = "#" + id;
+
+	existing_circles = d3.select("#vthings").selectAll("circle");
+	console.log("current # of things = " + existing_circles.size());
+
+	x_increment = (500 - 50) / (existing_circles.size() + 2);
+	x_next = x_increment + 50;
+
+	Template.vthing._draw_existing(existing_circles, x_next, x_increment);
+
+	x_next = x_next + (x_increment * existing_circles.size());
+
+	circle = d3.select("#vthings").selectAll(selector_id)
+	circle_data = circle.data([id]);
+	circle_data.enter()
+					.append("circle")
+					.style("stroke", "gray")
+					.style("fill", "white")
+					.attr("r", 40)
+					.attr("cx", function(d) {
+						i = x_next;
+						x_next = x_next + x_increment;
+						console.log("new circle at x = " + i);
+						return i
+					})
+					.attr("cy", function(d) { return 2 * 50})
+					.text(this.name)
+					.classed("thing", true)
+					.attr("id", function(d) { return d })
+					.on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
+					.on("mouseout", function(){d3.select(this).style("fill", "white");})
+					;
+
+	//var num_circles = 0;
+	//circle_data.attr("cx", function(d) {
+		//console.log("circle_data d = " + d + " num_circles = " + num_circles);
+		//num_circles = num_circles + 1;
+		//return num_circles * 50
+	//});
+}
+
+d3.selection.prototype.size = function() {
+	var n = 0;
+	this.each(function() { ++n; });
+	return n;
+};
--- simple-d3-with-meteor.js
+++ simple-d3-with-meteor.js
@@ -1,4 +1,5 @@
 Count = new Meteor.Collection("count");
+Things = new Meteor.Collection("things");
 
 
 if (Meteor.isClient) {
@@ -21,7 +22,55 @@ if (Meteor.isClient) {
       Count.update(c._id, {$inc: {number: -1}});
 		}
 	});
-			
+
+	Template.things.things = function() {
+		return Things.find({});
+	}
+
+	Template.vthings.vthings = function() {
+		return Things.find({});
+	}
+	Template.vthing.cx = function() {
+		return 50;
+	}
+
+	Template.vthing.cy = function() {
+		return 50;
+	}
+
+	Things.find({}).observeChanges({
+		removed: function(id) {
+			console.log("In client, Thing removed " + id );
+			var id = "a" + id;
+			var selector_id = "#" + id;
+
+			var removing_circle = d3.select("#vthings").selectAll(selector_id);
+			removing_circle.remove();
+
+			existing_circles = d3.select("#vthings").selectAll("circle");
+			console.log("new # of things = " + existing_circles.size());
+
+			x_increment = (500 - 50) / (existing_circles.size() + 1);
+			x_next = x_increment + 50;
+
+			Template.vthing._draw_existing(existing_circles, x_next, x_increment);
+		}
+	});
+
+	Template.vthing._draw_existing = function(existing_circles, x_next, x_increment) {
+		existing_circles
+				.transition()
+				.duration(750)
+				.style("stroke", "gray")
+				.attr("cx", function(d) {
+					i = x_next;
+					x_next = x_next + x_increment;
+					console.log("move existing circle to x = " + i);
+					return(i);
+				});
+	}
+
+
 }
 
 if (Meteor.isServer) {
@@ -29,6 +78,11 @@ if (Meteor.isServer) {
     // code to run on server at startup
     if (Count.find().count() === 0) {
 			Count.insert({number: 1});
+		}
+
+		if(Things.find().count() === 0) {
+			Things.insert({name: "thing 1" });
+			Things.insert({name: "thing 2" });
 		}
   });
 }
